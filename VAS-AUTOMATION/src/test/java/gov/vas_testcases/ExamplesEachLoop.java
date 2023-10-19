f59af9554b7620aff3b16c1baf2f60466231492b
mohammed bhuiyan <Mohammed.Bhuiyan6@va.gov>
2023-10-19 17:34:50 -0400

    First Commit in Automate Branch

---
+++ VAS-AUTOMATION/src/test/java/gov/vas_testcases/ExamplesEachLoop.java
@@ -0,0 +1,15 @@
+package gov.vas_testcases;
+
+public class ExamplesEachLoop {
+
+	public static void main(String[] args) {
+		     
+	        	String[] colors = {"red", "blue", "green", "white","black"};
+	        	
+	         	for (String arr : colors) {
+	           	  System.out.println(arr);
+	    	}
+
+	}
+
+}
